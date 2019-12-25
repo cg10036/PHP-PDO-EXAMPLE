185919751c1a69fb4692a19a68548f6afb3fc332
cg10036 <30382481+cg10036@users.noreply.github.com>
2019-12-26 04:45:25 +0900

    Create config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+$db_id = "DB_ID";
+$db_pw = "DB_PW";
+$db_host = "localhost";
+$db_port = "3306";
+$db_name = "DB_NAME";
+$db_charset = "utf8";
+$db_dsn = "mysql:host={$db_host};port={$db_port};dbname={$db_name};charset={$db_charset}";
+?>
